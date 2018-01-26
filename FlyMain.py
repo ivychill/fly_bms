3f8dc6a697afe819cd455a2c55935f55fb7fbfe5
feng.chen <fengchen@kuang-chi.com>
2018-01-26 15:15:28 +0800

    add 2 main

--- FlyMain.py
+++ FlyMain.py
@@ -9,7 +9,10 @@ if __name__ == "__main__":
     thread_fly = threading.Thread(target = FlyCtrl.random_fly)
     thread_fly.start()
     # time.sleep(0.1)
+    FlyCtrl.bms_interface.init_bms_control()
     while True:
         FlyCtrl.start()
+        FlyCtrl.bms_interface.send_ctrl_cmd('1')
         time.sleep(100)
+        FlyCtrl.bms_interface.send_ctrl_cmd('2')
         FlyCtrl.stop()
